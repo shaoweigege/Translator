bdcd7507a8f0024f0d778160b94c95143b1721fb
Fırat Kaya <48600092+benfiratkaya@users.noreply.github.com>
2019-03-23 17:23:51 +0300

    Update Translator.php

--- src/Translator.php
+++ src/Translator.php
@@ -14,13 +14,14 @@
  		private $file;
  		private $text;
  		
- 		public function __construct($exception = null, $type=null, $lang=null, $path=null) {
+ 		public function __construct($exception = null, $type = null, $lang = null, $path = null) {
  			$this->exception = (isset($exception)) ? $exception : false;
  			$this->type = (isset($type)) ? $type : 'json';
  			$this->lang = (isset($lang)) ? $lang : 'en_US';
  			$this->path = (isset($path)) ? rtrim($path, '/') : __DIR__.'/languages';
  			$this->setFile();
  		}
+ 		
  		private function setFile() {
  			$supportedTypes = array('php', 'json', 'xml', 'ini', 'yaml');
  			if (in_array($this->type, $supportedTypes)) {
@@ -30,37 +31,39 @@
  					if ($this->type === 'json') {
  						$this->text = json_decode($fileContent, true);
  					}
- 				}
- 				else {
+ 				} else {
 				 	if ($this->exception === true) {
  						throw new Exception('File not found!');
  					}
  					return false;
- 				}
- 			}
- 			else {
+ 				} else {
  				if ($this->exception === true) {
  					throw new Exception('This type is not supported!');
  				}
  				return false;
  			}
  		}
+ 		
  		public function setType($type = null) {
  			$this->type = (isset($type)) ? $type : 'json';
  			$this->setFile();
  		}
+ 		
  		public function setLang($lang = null) {
  			$this->lang = (isset($lang)) ? $lang : 'en_US';
  			$this->setFile();
  		}
+ 		
  		public function setPath($path = null) {
  			$this->path = (isset($path)) ? rtrim($path, '/') : __DIR__.'/languages';
  			$this->setFile();
  		}
+ 		
  		public function setException($exception = null) {
  			$this->exception = (isset($exception)) ? $exception : false;
  			$this->setFile();
  		}
+ 		
  		public function translate($text = null, $variable = null) {
  			if (isset($variable)) {
  				return strtr(((isset($this->text[$text])) ? $this->text[$text] : $text), $variable);
@@ -68,4 +71,3 @@
  			return ((isset($this->text[$text])) ? $this->text[$text] : $text);
   		}
  	}
-    
